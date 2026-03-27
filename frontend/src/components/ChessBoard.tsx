d77c30a05eeb0a53112ba0a8a67f9c27c7fb9383
ghostcoderraj <officiallyaditya12@gmail.com>
2026-03-27 15:21:37 +0530

    added some frontend

--- frontend/src/components/ChessBoard.tsx
+++ frontend/src/components/ChessBoard.tsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import { Color, PieceSymbol, Square } from 'chess.js'
+import { useState } from 'react';
 
-export const ChessBoard = () => {
-  return (
-    <div>ChessBoard</div>
-  )
+
+export const ChessBoard = ({ board , socket}: {
+    board: ({
+        square: Square;
+        type: PieceSymbol;
+        color: Color;
+    } | null)[][];
+    socket: WebSocket;
+}) => {
+    const [from , setFrom] = useState(null);
+    const [to, setTo] = useState(null)
+    return <div className='text-white-200'>
+        {board.map((row, i) => {
+            return <div key={i} className='flex'>
+                {row.map((square,j) => {
+                return <div onClick={() => {
+                    if (!from) {
+                        setFrom(square);
+                    }else{
+                        setTo(square);
+                        socket.send(JSON.stringify({
+                            type:"move",
+                            payload: {
+                                from: from.square,
+                                to: square.square
+                            }
+                        }))
+                    }
+                }} key={j} className={`w-16 h-16 ${(i + j) % 2 === 0 ? 'bg-green-500' :
+                    'bg-white'}`}>
+                        <div className='w-full justify-center flex h-full'>
+                            <div className='h-full justify-center flex flex-col'>
+                                {square ? square.type : ""}
+                            </div>
+                        </div>
+                </div>
+        })}
+            </div>
+        })}
+    </div>
 }
 
--- frontend/src/screens/Game.tsx
+++ frontend/src/screens/Game.tsx
@@ -1,16 +1,61 @@
+import { Button } from "@/components/Button";
 import { ChessBoard } from "@/components/ChessBoard"
+import { useSocket } from "@/hooks/useSocket";
+import { useEffect , useState } from "react";
+import { Chess } from "chess.js";
+
+/// TODO: Move together , there's code repetion here
+export const INIT_GAME = "init_game";
+export const MOVE = "move";
+export const GAME_OVER = "game_over"
 
 export const Game = () => {
+    const socket = useSocket();
+    const [chess, setchess] = useState(new Chess());
+    const [board , setBoard] = useState(chess.board());
+
+
+    useEffect(() => {
+        if (!socket) {
+            return;
+        }
+        socket.onmessage = (event) => {
+            const message = JSON.parse(event.data);
+            console.log(message);
+            switch (message.type) {
+                case INIT_GAME:
+                    setchess(new Chess())
+                    setBoard(chess.board())
+                    console.log('Game initialized');
+                    break;
+                case MOVE:
+                    const move = message.payload;
+                    chess.move(move)
+                    setBoard(chess.board())
+                    console.log("Move made")
+                    break;
+                
+            }
+        }
+    },[socket])
+
+    if(!socket) return <div>Connecting............</div>
+
+
     return <div className="justify-center flex">
         <div className="pt-8 max-w-screen-lg">
             <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
                 <div className="col-span-4 bg-red-200 w-full">
-                    <ChessBoard/>
+                    <ChessBoard socket = {socket} board= {board}/>
                 </div>
-                <div className="col-span-2 bg-green-20 w-full">
-                    <button onClick={()=> {
-                        navigate("/game")
-                    }}>Play</button>
+                <div className="col-span-2 bg-slate-900 w-full flex justify-center">
+                    <div className="pt-8">
+                        <Button onClick={()=> {
+                            socket.send(JSON.stringify({
+                                type:"INIT_GAME"
+                            }))
+                        }}>Play</Button>
+                    </div>
                 </div>
             </div>
         </div>
